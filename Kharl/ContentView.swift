a78acb6e1ab4c2a3139939a4daefe48bdd80548b
oppooName <jmy813@naver.com>
2020-11-29 17:34:52 +0900

    Initial Commit

---
+++ Kharl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kharl
+//
+//  Created by Rose on 2020/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kharl/KharlApp.swift
@@ -0,0 +1,17 @@
+//
+//  KharlApp.swift
+//  Kharl
+//
+//  Created by Rose on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct KharlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
